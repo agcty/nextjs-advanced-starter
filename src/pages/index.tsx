7ae1b3aac0d1a61c6172f31f195626020a8974e7
Alex Gogl <alex@gogl.io>
2020-10-04 22:35:02 +0200

    add some temp tailwind config stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,10 +11,7 @@ export default function Home() {
 
       <main className="min-h-screen bg-gradient-to-b from-geist-50 via-geist-50 to-geist-100 flex justify-center py-24">
         <div>
-          <h1
-            className="font-bold mt-20 text-center leading-tight tracking-tight"
-            style={{ fontSize: "72px" }}
-          >
+          <h1 className="font-bold mt-20 text-center leading-none sm:leading-tight tracking-tight text-5xl sm:text-6xl">
             Next.js Template.
             <br />
             Advanced Starter.
@@ -25,60 +22,61 @@ export default function Home() {
             2020, no need to config absolute-imports, code-formatting & linting.
           </h2>
 
-          <section
-            className="bg-white rounded-lg mt-20 grid grid-cols-2"
-            style={{
-              width: "1000px",
-              minHeight: "350px",
-              boxShadow: "rgba(0, 0, 0, 0.12) 0px 30px 60px 0px",
-            }}
-          >
-            <div className="flex flex-col justify-center bg-geist-50 rounded-l-lg">
-              <FeatureList>
-                <Feature>
-                  Fast design workflow with <InfoText text="Tailwind CSS" />
-                </Feature>
-                <Feature>
-                  <InfoText text="TypeScript rules" /> that make sense
-                </Feature>
-                <Feature>
-                  Customizable <InfoText text="ESLint config" />
-                </Feature>
-                <Feature>
-                  <InfoText text="Auto code formatting" /> with Prettier
-                </Feature>
-                <Feature>
-                  Beautiful <InfoText text="Inter font" />
-                </Feature>
-                <Feature>
-                  Standardized <InfoText text="absolute imports" />{" "}
-                </Feature>
-                {/* <Feature text="TypeScript config" />
+          <div className="px-4 sm:px-0">
+            <section
+              className="bg-white rounded-lg mt-20 grid grid-cols-1 sm:grid-cols-2 w-full sm:w-1000"
+              style={{
+                minHeight: "350px",
+                boxShadow: "rgba(0, 0, 0, 0.12) 0px 30px 60px 0px",
+              }}
+            >
+              <div className="flex flex-col justify-center bg-geist-50 rounded-l-lg">
+                <FeatureList>
+                  <Feature>
+                    Fast design workflow with <InfoText text="Tailwind CSS" />
+                  </Feature>
+                  <Feature>
+                    <InfoText text="TypeScript rules" /> that make sense
+                  </Feature>
+                  <Feature>
+                    Customizable <InfoText text="ESLint config" />
+                  </Feature>
+                  <Feature>
+                    <InfoText text="Auto code formatting" /> with Prettier
+                  </Feature>
+                  <Feature>
+                    Beautiful <InfoText text="Inter font" />
+                  </Feature>
+                  <Feature>
+                    Standardized <InfoText text="absolute imports" />{" "}
+                  </Feature>
+                  {/* <Feature text="TypeScript config" />
                 <Feature text="ESLint config" />
                 <Feature text="Prettier" />
                 <Feature text="Inter Font" /> */}
-              </FeatureList>
-            </div>
+                </FeatureList>
+              </div>
 
-            <div className="place-self-center text-center space-y-5">
-              <h3 className="text-3xl font-bold">Get it 👇</h3>
+              <div className="place-self-center text-center space-y-5 py-24 px-4">
+                <h3 className="text-3xl font-bold">Get it 👇</h3>
 
-              <span className="inline-flex rounded-md shadow-sm">
-                <Link href="  https://github.com/agcty/nextjs-advanced-starter">
-                  <a
-                    type="button"
-                    className="inline-flex items-center px-10 py-4 border border-transparent text-base leading-6 rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition ease-in-out duration-150"
-                  >
-                    Copy Template from GitHub
-                  </a>
-                </Link>
-              </span>
+                <span className="inline-flex rounded-md shadow-sm">
+                  <Link href="  https://github.com/agcty/nextjs-advanced-starter">
+                    <a
+                      type="button"
+                      className="inline-flex items-center px-10 py-4 border border-transparent text-base leading-6 rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition ease-in-out duration-150"
+                    >
+                      Copy Template from GitHub
+                    </a>
+                  </Link>
+                </span>
 
-              <h2 className="text-gray-600 tracking-tight">
-                Used by devs around the world
-              </h2>
-            </div>
-          </section>
+                <h2 className="text-gray-600 tracking-tight">
+                  Used by devs around the world
+                </h2>
+              </div>
+            </section>
+          </div>
         </div>
       </main>
     </div>
@@ -94,7 +92,7 @@ function FeatureList({ children }: FeatureListProps) {
 
 function Feature({ children }) {
   return (
-    <li className="flex items-center">
+    <li className="flex items-center leading-relaxed">
       <CheckIcon className="w-5 h-5 p-1 rounded-full flex-shrink-0 text-gray-100 bg-blue-600" />
       <p className="ml-3 text-gray-600 text-lg">{children}</p>
     </li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,27 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   purge: ["./src/**/*.tsx"],
   theme: {
+    // todo add default line height https://tailwindcss.com/docs/font-size#app
+    fontSize: {
+      xs: ".75rem",
+      sm: ".875rem",
+      tiny: ".875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+      "7xl": "5rem",
+    },
     extend: {
       screens: {
         xxl: { min: "2000px" },
+      },
+      width: {
+        1000: "1000px",
       },
       fontSize: {
         ...defaultTheme.fontSize,
