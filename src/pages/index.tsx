9d1a0a811f1ab773cf3d7832ef9b24e70048cf24
Alex Gogl <alex@gogl.io>
2020-10-17 14:26:35 +0200

    better meta description and title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,12 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Next.js advanced start template.</title>
+        <meta
+          name="description"
+          content="Use tailwind css, eslint, prettier & absolute imports instantly.
+            Easily extendable zero-config template for pros and beginners."
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
