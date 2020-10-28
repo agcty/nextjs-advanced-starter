1fe141ef97b2a596fa00122fc9c23d8cd5d0f2d4
Alex Gogl <alex@gogl.io>
2020-10-28 22:23:40 +0100

    upgrade to next 10 and add postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,19 @@
-const plugins = {
-  "postcss-import": {},
-  tailwindcss: {},
-  autoprefixer: {},
-};
-
 module.exports = {
-  plugins,
+  plugins: [
+    "tailwindcss",
+    "postcss-import",
+    "postcss-flexbugs-fixes",
+    [
+      "postcss-preset-env",
+      {
+        autoprefixer: {
+          flexbox: "no-2009",
+        },
+        stage: 3,
+        features: {
+          "custom-properties": false,
+        },
+      },
+    ],
+  ],
 };
