df9c7ffefaabe4daf6be289f0cfb5757dd736f44
Alex <alex@gogl.io>
2021-05-09 20:04:26 +0200

    turn on tailwind jit mode by default (#11)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
+  mode: "jit",
   purge: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
