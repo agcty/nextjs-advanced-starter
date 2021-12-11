958686278c86cb231575524ca01b441c108373aa
Alex Gogl <alex@gogl.io>
2021-12-11 22:02:53 +0100

    remove darkmode false

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,6 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
