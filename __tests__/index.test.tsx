149b3ed2dde059dfe8a76f7a26c0f094cd89263d
Alex Gogl <alex@gogl.io>
2022-03-04 22:21:24 +0100

    now uses nextjs recommended jest setup

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,19 +1,14 @@
-/**
- * @jest-environment jsdom
- */
-
-import React from "react"
-
 import { render, screen } from "@testing-library/react"
 
-import Index from "../src/pages/index"
+import Home from "../src/pages/index"
 
-// this is not a very good test but it just shows that testing works
-it("Link with that points to github exists", () => {
-  render(<Index />, {})
+describe("Home", () => {
+  it("cta opens github", () => {
+    render(<Home />)
 
-  expect(screen.getByText("Copy Template from GitHub")).toHaveAttribute(
-    "href",
-    "https://github.com/agcty/nextjs-advanced-starter"
-  )
+    expect(screen.getByText("Copy Template from GitHub")).toHaveAttribute(
+      "href",
+      "https://github.com/agcty/nextjs-advanced-starter"
+    )
+  })
 })
